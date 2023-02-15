980cf9f441033cf4fa74dcc0824a0956940fdafb
Fausto Lavezzari <faustolavezzari99@gmail.com>
2023-02-15 20:12:57 -0300

    split in fuctions and ID from link trim

--- main.py
+++ main.py
@@ -2,27 +2,52 @@ from googleapiclient.discovery import build
 from google.oauth2 import service_account
 import pandas as pd
 
-SERVICE_ACCOUNT_FILE = 'credentials.json'
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-creds = None
-creds = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+link_spreadsheat = ''
+range = "hoja1!A1:C5"
+creds = "credentials.json"
 
+def main():
+    spreadsheet_id = getIDfromLink(link_spreadsheat)
+    gsheet = getSpreadsheet(creds,spreadsheet_id)
 
-# The ID spreadsheet.
-SAMPLE_SPREADSHEET_ID = 'spreadsheat id'
+    data = pd.DataFrame(gsheet[1:], columns= gsheet[0])
 
-service = build('sheets', 'v4', credentials=creds)
-
-# Call the Sheets API
-sheet = service.spreadsheets()
-result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                            range="hoja1!A1:D6").execute()
-
-values = result.get('values', [])
-
-info = pd.DataFrame(values[1:], columns= values[0])
-print(info)
+    print(data)
 
 
+def getSpreadsheet(service_account_file, spreadsheet_id):
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+    creds = None
+    creds = service_account.Credentials.from_service_account_file(
+            service_account_file, scopes=SCOPES)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=spreadsheet_id,
+                                range=range).execute()
+
+    values = result.get('values', [])
+
+    return values
+
+def getIDfromLink(link):
+    start = link.find("/d/")+3
+
+    if(start == -1):
+        exit("Invalid Link")
+
+    id = link[start: ]
+    end = id.find("/")
+
+    if(end == -1):
+        exit("Invalid Link")
+
+    id = id[:end]
+
+    return id
+
+if __name__ == '__main__':
+    main()
